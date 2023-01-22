d4d7aab6d24d62360f1da7200a009c9c31c90ece
Musa Demir <alpermusademir@gmail.com>
2023-01-22 13:47:09 +0300

    Readme updated.

--- public/js/resources_assets_js_pages_MessagesComponent_vue.js
+++ public/js/resources_assets_js_pages_MessagesComponent_vue.js
@@ -113,7 +113,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.messages-area[data-v-09c75824] {\n  height: 500px;\n  overflow-y: auto;\n  border-bottom: 1px solid #d3e0e9;\n  margin-bottom: 10px;\n}\n.action-area[data-v-09c75824] {\n  margin-bottom: 10px;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.messages-area[data-v-09c75824] {\r\n  height: 500px;\r\n  overflow-y: auto;\r\n  border-bottom: 1px solid #d3e0e9;\r\n  margin-bottom: 10px;\n}\n.action-area[data-v-09c75824] {\r\n  margin-bottom: 10px;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
